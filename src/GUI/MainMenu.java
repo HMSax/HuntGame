11a70327b1c504e33a799b64ea8492974191792a
Mattias Seth <mattias.seth@gmail.com>
2023-12-14 12:13:22 +0100

    Added quitbutton + actionlistner

--- src/GUI/MainMenu.java
+++ src/GUI/MainMenu.java
@@ -2,36 +2,50 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainMenu extends JFrame {
 
     JPanel borderPanel = new JPanel();
     JPanel menuPanel = new JPanel();
-    JButton b = new JButton("New Game");
-    JButton b2 = new JButton("B2");
-    JButton b3 = new JButton("B3");
-    JButton b4 = new JButton("B4");
-    JButton b5 = new JButton("QUIT");
+    JButton newGameButton = new JButton("New Game");
+    JButton b2 = new JButton("Button 2");
+    JButton b3 = new JButton("Button 3");
+    JButton b4 = new JButton("Button 4");
+    JButton quitButton = new JButton("QUIT");
 
     public MainMenu() {
+        // ActionListeners
+        ActionListener exitListener = new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                System.exit(0);
+            }
+        };
+
         // Panels
         this.add(borderPanel);
         borderPanel.setLayout(new BorderLayout());
         borderPanel.add(menuPanel, BorderLayout.CENTER);
         menuPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
 
-        // buttons
-        b.setPreferredSize(new Dimension(200, 50));
+        // button size
+        newGameButton.setPreferredSize(new Dimension(200, 50));
         b2.setPreferredSize(new Dimension(200, 50));
         b3.setPreferredSize(new Dimension(200, 50));
         b4.setPreferredSize(new Dimension(200, 50));
-        b5.setPreferredSize(new Dimension(200, 50));
+        quitButton.setPreferredSize(new Dimension(200, 50));
 
-        menuPanel.add(b);
+        // Add buttons to panel
+        menuPanel.add(newGameButton);
         menuPanel.add(b2);
         menuPanel.add(b3);
         menuPanel.add(b4);
-        menuPanel.add(b5);
+        menuPanel.add(quitButton);
+
+        // add ActionListeners to buttons
+        quitButton.addActionListener(exitListener);
 
         setTitle("HuntGame");
         setSize(300, 320);
