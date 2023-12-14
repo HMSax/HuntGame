077bdb51bba11601ef86ff75492271e86c225f13
Mattias Seth <mattias.seth@gmail.com>
2023-12-14 13:21:26 +0100

    Added CardLayout, go back button and instructionsPanel

--- src/GUI/MainMenu.java
+++ src/GUI/MainMenu.java
@@ -7,46 +7,64 @@ import java.awt.event.ActionListener;
 
 public class MainMenu extends JFrame {
 
-    JPanel borderPanel = new JPanel();
-    JPanel menuPanel = new JPanel();
-    JButton newGameButton = new JButton("New Game");
-    JButton b2 = new JButton("Button 2");
-    JButton b3 = new JButton("Button 3");
-    JButton b4 = new JButton("Button 4");
-    JButton quitButton = new JButton("QUIT");
+    // CardLayout
+    private JPanel cardPanel = new JPanel(new CardLayout());
+
+    // Main menu panel
+    private JPanel menuPanel = new JPanel();
+    private JButton newGameButton = new JButton("New Game");
+    private JButton b2 = new JButton("Button 2");
+    private JButton b3 = new JButton("Button 3");
+    private JButton b4 = new JButton("Game Instructions");
+
+
+
+    // Instruction panel.
+    private JPanel instructionsPanel = new JPanel();
+    private JTextArea instructionsText = new JTextArea("TExt:\n Text: \n Text: \n Text:");
+
+    // Go back button and quit button (can resue)
+    private JButton goBackButton = new JButton("Go Back");
+    private JButton quitButton = new JButton("QUIT");
 
     public MainMenu() {
         // ActionListeners
-        ActionListener exitListener = new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                System.exit(0);
-            }
-        };
+        ActionListener exitListener = ae -> System.exit(0);
+        ActionListener instructionsListener = ae -> showInstructionsPanel();
+        ActionListener goBackListener = ae -> showMenuPanel();
 
-        // Panels
-        this.add(borderPanel);
-        borderPanel.setLayout(new BorderLayout());
-        borderPanel.add(menuPanel, BorderLayout.CENTER);
+        // Menu panel
         menuPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-
-        // button size
+        menuPanel.add(newGameButton);
+        menuPanel.add(b2);
+        menuPanel.add(b3);
+        menuPanel.add(b4);
+        menuPanel.add(quitButton);
         newGameButton.setPreferredSize(new Dimension(200, 50));
         b2.setPreferredSize(new Dimension(200, 50));
         b3.setPreferredSize(new Dimension(200, 50));
         b4.setPreferredSize(new Dimension(200, 50));
         quitButton.setPreferredSize(new Dimension(200, 50));
 
-        // Add buttons to panel
-        menuPanel.add(newGameButton);
-        menuPanel.add(b2);
-        menuPanel.add(b3);
-        menuPanel.add(b4);
-        menuPanel.add(quitButton);
+        // Instructions panel
+        instructionsPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        instructionsPanel.add(instructionsText);
+        instructionsPanel.add(goBackButton);
+
+        // Add panels to cardPanel
+        cardPanel.add(menuPanel, "menu");
+        cardPanel.add(instructionsPanel, "instructions");
 
         // add ActionListeners to buttons
         quitButton.addActionListener(exitListener);
+        b4.addActionListener(instructionsListener);
+        goBackButton.addActionListener(goBackListener);
 
+        //
+        showMenuPanel();
+        add(cardPanel);
+
+        //
         setTitle("HuntGame");
         setSize(300, 320);
         setVisible(true);
@@ -54,9 +72,20 @@ public class MainMenu extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             MainMenu m = new MainMenu();
         });
+    }
+
+    private void showInstructionsPanel() {
+        CardLayout cardLayout = (CardLayout) cardPanel.getLayout();
+        cardLayout.show(cardPanel, "instructions");
+    }
+
+    private void showMenuPanel() {
+        CardLayout cardLayout = (CardLayout) cardPanel.getLayout();
+        cardLayout.show(cardPanel, "menu");
     }
 }
